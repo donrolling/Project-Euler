828d77d76d65e244b84a1d076d0e5ad8aedf97d9
Don Rolling <don.rolling@pcg.com>
2017-12-04 16:10:55 -0600

    solved #25. removed F# code because it was erroring.

---
+++ CSharp/Problems/Problem26.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp.Helpers;
+
+namespace CSharp.Problems {
+	public class Problem26 : IProblem {
+		//Reciprocal cycles
+		//Problem 26
+		//A unit fraction contains 1 in the numerator.The decimal representation of the unit fractions with denominators 2 to 10 are given:
+
+		//1/2	= 	0.5
+		//1/3	= 	0.(3)
+		//1/4	= 	0.25
+		//1/5	= 	0.2
+		//1/6	= 	0.1(6)
+		//1/7	= 	0.(142857)
+		//1/8	= 	0.125
+		//1/9	= 	0.(1)
+		//1/10	= 	0.1
+		//Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle.It can be seen that 1/7 has a 6-digit recurring cycle.
+
+		//Find the value of d< 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+
+		public string GetAnswer() {
+			var answer = solve();
+			return Label.GetLabel(this.GetType(), answer.ToString());
+		}
+
+		private BigInteger solve() {
+			var result = 0;
+			int d = 2;
+			decimal x = 0;
+			do {
+				x = (decimal)1 / d;
+				d++;
+				Console.WriteLine(x);
+			} while (d < 11);
+			return result;
+		}
+	}
+}
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -5,11 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CSharp {
-	class Program {
-		static void Main(string[] args) {
+	internal class Program {
+
+		private static void Main(string[] args) {
 			var startTime = DateTime.Now;
 			var message = "{0}\nElapsed Time (seconds): {1}";
-			var answer = new Problems.Problem24().GetAnswer();
+			var answer = new Problems.Problem26().GetAnswer();
 			var endTime = DateTime.Now;
 			var diff = (endTime - startTime).TotalSeconds;
 			Console.WriteLine(string.Format(message, answer, diff.ToString()));
